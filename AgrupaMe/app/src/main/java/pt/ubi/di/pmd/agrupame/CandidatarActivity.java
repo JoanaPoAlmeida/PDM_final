84bd62e2f0348be10f1fa26c099c7c1c575e9ad1
JoanaPoAlmeida <58987530+JoanaPoAlmeida@users.noreply.github.com>
2021-12-20 01:31:17 +0000

    Design da app de users acabado
    Falta adicionar conteúdo nas atividades: Candidatar, Candidaturas e Equipas

--- AgrupaMe/app/src/main/java/pt/ubi/di/pmd/agrupame/CandidatarActivity.java
+++ AgrupaMe/app/src/main/java/pt/ubi/di/pmd/agrupame/CandidatarActivity.java
@@ -2,13 +2,30 @@ package pt.ubi.di.pmd.agrupame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CandidatarActivity extends AppCompatActivity {
 
+    private TextView tvBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidatar);
+
+        tvBack = (TextView) findViewById(R.id.tvBack);
+        tvBack.setOnClickListener(this::OnClick);
     }
+
+    public void OnClick(View v){
+        switch(v.getId()){
+            case R.id.tvBack:
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                break;
+        }
+
+    }
+
 }
